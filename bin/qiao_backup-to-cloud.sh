ce017821c97e4c2ff677939e865da06af177e37c
qiao <qiao.helloworld@gmail.com>
2016-10-20 23:22:11 +0800

    add backup-to-cloud script

---
+++ bin/qiao_backup-to-cloud.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROJECT_DIR=${HOME}/projects
+ROBOTVISIONLIB_DIR=${PROJECT_DIR}/robotvisionlib
+ROBOTVISIONAPP_DIR=${PROJECT_DIR}/robotvisionapp
+QIAO_BIN_DIR=${HOME}/Tools/bin
+
+cd ${ROBOTVISIONLIB_DIR}
+git stash clear
+git stash save
+git push bitbucket --all --force
+git stash pop
+
+cd ${ROBOTVISIONAPP_DIR}
+git stash clear
+git stash save
+git push bitbucket --all --force
+git stash pop
+
+cd ${QIAO_BIN_DIR}
+git stash clear
+git stash save
+git push --all --force
+git stash pop
