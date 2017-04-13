e93bfd39829dd056ff17a249fb267ffe28cef2bd
qiao <qiao.helloworld@gmail.com>
2017-04-13 17:58:25 +0800

    add new repo to backup

--- bin/qiao_backup-to-cloud.sh
+++ bin/qiao_backup-to-cloud.sh
@@ -3,6 +3,7 @@
 PROJECT_DIR=${HOME}/projects
 ROBOTVISIONLIB_DIR=${PROJECT_DIR}/robotvisionlib
 ROBOTVISIONAPP_DIR=${PROJECT_DIR}/robotvisionapp
+ROBOTFRAMEWORK_DIR=${PROJECT_DIR}/robotframework
 QIAO_BIN_DIR=${HOME}/Tools/bin
 
 cd ${ROBOTVISIONLIB_DIR}
@@ -12,6 +13,12 @@ git push gitlab --all --force
 git stash pop
 
 cd ${ROBOTVISIONAPP_DIR}
+git stash clear
+git stash save
+git push gitlab --all --force
+git stash pop
+
+cd ${ROBOTFRAMEWORK_DIR}
 git stash clear
 git stash save
 git push gitlab --all --force
