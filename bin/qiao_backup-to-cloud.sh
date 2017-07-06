14b3e7ef7eb6c46fca0907364f7eb2e52eadd7d1
qiao <qiao.helloworld@gmail.com>
2017-07-06 10:29:22 +0800

    add more dirs to backup

--- bin/qiao_backup-to-cloud.sh
+++ bin/qiao_backup-to-cloud.sh
@@ -2,8 +2,11 @@
 
 PROJECT_DIR=${HOME}/projects
 ROBOTVISIONLIB_DIR=${PROJECT_DIR}/robotvisionlib
-ROBOTVISIONAPP_DIR=${PROJECT_DIR}/robotvisionapp
+ROBOTVISIONSRV_DIR=${PROJECT_DIR}/robotvisionservice
+ROBOTTRANSFORM_DIR=${PROJECT_DIR}/transformation
+ROBOTVISIONOPS_DIR=${PROJECT_DIR}/cv_devops
 ROBOTFRAMEWORK_DIR=${PROJECT_DIR}/robotframework
+
 QIAO_BIN_DIR=${HOME}/Tools/bin
 
 cd ${ROBOTVISIONLIB_DIR}
@@ -12,7 +15,19 @@ git stash save
 git push gitlab --all --force
 git stash pop
 
-cd ${ROBOTVISIONAPP_DIR}
+cd ${ROBOTVISIONSRV_DIR}
+git stash clear
+git stash save
+git push gitlab --all --force
+git stash pop
+
+cd ${ROBOTTRANSFORM_DIR}
+git stash clear
+git stash save
+git push gitlab --all --force
+git stash pop
+
+cd ${ROBOTVISIONOPS_DIR}
 git stash clear
 git stash save
 git push gitlab --all --force
