76f8ad8cc833af1e02de77fca924a015861045c7
qiao <qiao.helloworld@gmail.com>
2017-11-23 13:08:10 +0800

    update docker of opengrok

--- docker/opengrok/run_container.sh
+++ docker/opengrok/run_container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+OPENGROK_SRC=${BASE_DIR}/opengrok_src
+OPENGROK_DATA=${BASE_DIR}/opengrok_data
+
 docker run -td --rm \
   --privileged=true \
-  --volume="$1:/src" \
+  --volume="${OPENGROK_SRC}:/src" \
+  --volume="${OPENGROK_DATA}:/data" \
   -p 8080:8080 \
   opengrok
-  #-v /path/to/opengrok_data:/data \
