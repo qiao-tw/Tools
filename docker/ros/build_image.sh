991a6c771a9e030941d3476f46ed9f2684728f4c
qiao <qiao.helloworld@gmail.com>
2019-05-21 21:42:12 +0800

    Add a docker as my develop env

---
+++ docker/ros/build_image.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -o errexit
+set -o verbose
+
+# CERES_SOLVER_VERSION="1.13.0"
+# if [ -d "ceres-solver" ]; then
+#     (cd ceres-solver; git checkout tags/${CERES_SOLVER_VERSION})
+# else
+#     git clone https://ceres-solver.googlesource.com/ceres-solver
+# fi
+#
+# PROTOBUF_VERSION="v3.4.1"
+# if [ -d "protobuf" ]; then
+#     (cd protobuf; git checkout tags/${PROTOBUF_VERSION})
+# else
+#     git clone https://github.com/google/protobuf.git
+# fi
+
+docker build                \
+  -t devel-env              \
+  --build-arg USER=$USER    \
+  --build-arg UID=$(id -u)  \
+  --build-arg GID=$(id -g)  \
+  .
--- docker/ros/run_container.sh
+++ docker/ros/run_container.sh
@@ -1,8 +1,76 @@
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-xhost + local:root
-docker run -it \
-    -e DISPLAY=$(ipconfig getifaddr en0):0 \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    osrf/ros:indigo-desktop-full #\
-    #    --env="QT_X11_NO_MITSHM=1" \
-#    rqt
+#!/bin/sh
+
+set -o errexit
+set -o verbose
+# pulled from gpal/ros_devel/xauth.sh
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+
+XAUTH=/tmp/.docker.xauth
+if [ -d $XAUTH ]
+then
+    sudo rm -rf $XAUTH
+fi
+
+if [ ! -f $XAUTH ]
+then
+    touch $XAUTH
+    xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    fi
+    chmod a+r $XAUTH
+fi
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+  xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
+  if [ ! -z "$xauth_list" ]
+  then
+    echo $xauth_list | xauth -f $XAUTH nmerge -
+  else
+    touch $XAUTH
+  fi
+  chmod a+r $XAUTH
+fi
+
+# socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+xhost +
+
+IMAGE_NAME=devel-env
+CONTAINER_NAME=devel-env
+DOCKER_RUN_PARAMETER_LIST="-it \
+  --user $(id -u) \
+  --name=$CONTAINER_NAME \
+  --tmpfs /tmp:exec \
+  -h docker \
+  -e DISPLAY=host.docker.internal:0 \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -e USER=$USER \
+  -v /dataset:/dataset \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/group:/etc/group:ro \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/shadow:/etc/shadow:ro \
+  -v /etc/sudoers.d:/etc/sudoers.d:ro \
+  -v /home/$USER:/home/$USER \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $XAUTH:$XAUTH \
+  -v /run/udev:/run/udev:ro \
+  -v /dev:/dev \
+  --net=host \
+  --privileged \
+  -d \
+  ${IMAGE_NAME} \
+  /bin/bash"
+  #-e XAUTHORITY=$XAUTH \
+  #-v "$WORKSPACE_DIR":/home/gpal \
+
+echo $DOCKER_RUN_PARAMETER_LIST
+
+docker run $DOCKER_RUN_PARAMETER_LIST
+
+docker exec -it $CONTAINER_NAME /bin/bash
